6c8661153ed7df735a91fb6ce1ff06644209507e
Razvan Litianu <litianu.razvan@gmail.com>
2026-05-26 12:19:27 +0300

    Bugfix FXIOS-15893 Fix retain cycle, dead code, and force unwrap
    Use [weak tab] in onNextCommit closure to avoid retain cycle if
    navigation is cancelled. Remove unused hadHandler variable left from
    debug logging. Replace force unwrap with if-let binding in
    persistTranslationConfig.

--- firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+WebViewDelegates.swift
+++ firefox-ios/Client/Frontend/Browser/BrowserViewController/Extensions/BrowserViewController+WebViewDelegates.swift
@@ -509,8 +509,8 @@ extension BrowserViewController: WKNavigationDelegate {
             let savedConfig = tab.savedTranslation(for: currentItem)
 
             if let savedConfig {
-                tab.onNextCommit = {
-                    tab.translationConfiguration = savedConfig
+                tab.onNextCommit = { [weak tab] in
+                    tab?.translationConfiguration = savedConfig
                 }
             } else {
                 tab.translationConfiguration = nil
@@ -1188,7 +1188,6 @@ extension BrowserViewController: WKNavigationDelegate {
         searchTelemetry.trackTabAndTopSiteSAP(tab, webView: webView)
         webviewTelemetry.start()
         tab.url = webView.url
-        let hadHandler = tab.onNextCommit != nil
         if let handler = tab.onNextCommit {
             tab.onNextCommit = nil
             handler()
